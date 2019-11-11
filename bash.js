fc333266f48c4e726747c3c5768a48eec79ad966
Laura Nemeth <lauranemeth.pr@gmail.com>
2019-11-11 12:29:27 -0500

    new pwd file

--- bash.js
+++ bash.js
@@ -1,9 +1,2 @@
-process.stdout.write('prompt >');
-
-process.stdin.on('process',(process) => {
-    //const cmd = data.toString().trim();
-      const cwd = process.cwd()
-    process.stdout.write('You typed: ' + cwd)
-    process.stdout.write('\nprompt > ');
-});
-
+const pwd = require('./pwd')
+pwd()
---
+++ pwd.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    process.stdout.write('prompt >');
+
+process.stdin.on('process',(process) => {
+    //const cmd = data.toString().trim();
+        const cwd = process.cwd()
+    process.stdout.write('You typed: ' + cwd)
+    process.stdout.write('\nprompt > ');
+});
+}
