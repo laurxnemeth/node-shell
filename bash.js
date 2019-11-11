461603c66afec256bab3a123532bdf4558879574
Beth Howell <beahowel117@gmail.com>
2019-11-11 12:12:22 -0500

    cwd

--- bash.js
+++ bash.js
@@ -1,8 +1,9 @@
 process.stdout.write('prompt >');
 
-process.stdin.on('data',(data) => {
-    const cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd)
+process.stdin.on('process',(process) => {
+    //const cmd = data.toString().trim();
+      const cwd = process.cwd()
+    process.stdout.write('You typed: ' + cwd)
     process.stdout.write('\nprompt > ');
 });
+
